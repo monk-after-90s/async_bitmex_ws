21834bfeaaccfcc892d03c226ddecc381d85b9f5
90houlaoheshang <907333918@qq.com>
2020-07-09 10:57:00 +0800

    改包名

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="async-bitmex-ws",  # Replace with your own username
+    name="async_bitmex_ws",
     version="0.9.9",
     author="Antas",
     author_email="",
