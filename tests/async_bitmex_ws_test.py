cc485bcee5dd94befec819a7005dfd06726206f6
90houlaoheshang <907333918@qq.com>
2020-07-10 17:02:02 +0800

    使用asyncUnittest库测试pingpong

--- tests/async_bitmex_ws_test.py
+++ tests/async_bitmex_ws_test.py
@@ -1,54 +1,39 @@
-'''import asyncio
-
-import pytest
-
-
-async def say(what, when):
-    await asyncio.sleep(when)
-    return what
-
-
-@pytest.mark.asyncio
-async def test_say():
-    assert 'Hello!' == await say('Hello!', 0)
-'''
+import asyncUnittest
+from asyncUnittest import AsyncTestCase
+from async_bitmex_ws import AsyncBitMEXWebsocket
 import asyncio
-
-import sys
-import os
-from aiounittest import AsyncTestCase
-import unittest
-
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from loguru import logger
 
 
-class MyTest(AsyncTestCase):
+class TestPingPong(AsyncTestCase):
+    ws: AsyncBitMEXWebsocket = None
 
     @classmethod
-    def setUpClass(cls) -> None:
-        cls.a = 10
+    async def setUpClass(cls) -> None:
+        cls.ws = await AsyncBitMEXWebsocket.create_instance(api_key='IGMzIdy55DWYGKVSFIECBQjy',
+                                                            api_secret='HB7z6vhXvu9fSJrcYnjWm6R_9JhSs6dVYwKPQryadRSG8atF',
+                                                            testnet=True)
 
-    def setUp(self) -> None:
-        self.b = 3
+    async def test_ping_pong(self):
+        n = 3
+        last_pong_time = 0
+        async for news in self.ws.new_message_watcher():
+            if news == 'pong':
+                if last_pong_time:
+                    interval_time = round(asyncio.get_running_loop().time() - last_pong_time)
+                    logger.info(interval_time)
 
-    async def test_sample(self):
-        await asyncio.sleep(0)
-        print(type(self).a)
-        print(self.b)
-        type(self).a += 1
-        self.b += 1
-        print(type(self).a)
-        print(self.b)
+                    assert interval_time == 5
+                    n -= 1
+                    if n == 0:
+                        break
 
-    async def test_sample2(self):
-        await asyncio.sleep(0)
-        print(type(self).a)
-        print(self.b)
-        type(self).a += 1
-        self.b += 1
-        print(type(self).a)
-        print(self.b)
+                last_pong_time = asyncio.get_running_loop().time()
+
+    @classmethod
+    async def tearDownClass(cls) -> None:
+        await cls.ws.exit()
 
 
 if __name__ == '__main__':
-    unittest.main()
+    asyncUnittest.run()
