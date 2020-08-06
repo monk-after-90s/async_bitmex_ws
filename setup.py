6bb2823efd56e9b84fade965cb873a027e9340e0
90houlaoheshang <907333918@qq.com>
2020-08-06 14:04:34 +0800

    更新版本

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="async_bitmex_ws",
-    version="1.0.0",
+    version="1.0.1",
     author="Antas",
     author_email="",
     description="Asynchronous bitmex websocket api development kit",
